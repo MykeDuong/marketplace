5305c2dfd2a9c837af6d13cbd8ecbcc1ca3317a0
MykeDuong <hongminh4402@gmail.com>
2022-06-15 21:28:09 -0700

    Made the layout for the home screen

---
+++ components/NFTCard.js
@@ -0,0 +1,24 @@
+import { View, Image, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import React from 'react'
+
+import { COLORS, SIZES, SHADOWS, assets } from '../constants'
+
+const NFTCard = ({ data }) => {
+
+    const navigation = useNavigation();
+
+    return (
+        <View style={{
+            backgroundColor: COLORS.white,
+            borderRadius: SIZES.font,
+            marginBottom: SIZES.extraLarge,
+            margin: SIZES.base,
+            ...SHADOWS.dark
+        }}>  
+            <Text>NFTCard</Text>
+        </View>
+    )
+}
+
+export default NFTCard
